030dc1a8812843eacc9e253b217436b6072201a3
Alvaro Santillan <afsm1995@live.com>
2020-02-17 01:29:08 -0700

    Breath First Search Complete Unvarified

--- Snake/pathfinding.playground/Contents.swift
+++ Snake/pathfinding.playground/Contents.swift
@@ -1,90 +1,96 @@
-struct MyPoint {
+struct Duple {
     var x: Int
     var y: Int
 }
 
-extension MyPoint: Hashable {
+extension Duple: Hashable {
     public var hashValue: Int {
         return x.hashValue ^ y.hashValue
     }
 
-    public static func == (lhs: MyPoint, rhs: MyPoint) -> Bool {
+    public static func == (lhs: Duple, rhs: Duple) -> Bool {
         return lhs.x == rhs.x && lhs.y == rhs.y
     }
 }
 
-func matrixToDictionary(mazze: Array<Array<Int>>) -> Dictionary<MyPoint, Dictionary<MyPoint, Int>> {
-    var mazeDistances = [MyPoint : [MyPoint : Int]]()
-    var vaildValues = [MyPoint : Int]()
+func matrixToDictionary(mazze: Array<Array<Int>>) -> Dictionary<Duple, Dictionary<Duple, Int>> {
+    var mazeDistances = [Duple : [Duple : Int]]()
+    var vaildValues = [Duple : Int]()
 
     for(y, innerArray) in maze.enumerated() {
         for(x, _) in innerArray.enumerated() {
             if ((mazze[y][x]) == 0) {
                 // Up
                 if ((mazze[y-1][x]) == 0) {
-                    vaildValues[MyPoint(x: x, y: y-1)] = 1
+                    vaildValues[Duple(x: x, y: y-1)] = 1
                 }
                 // Right
                 if (mazze[y][x+1] == 0) {
-                    vaildValues[MyPoint(x: x+1, y: y)] = 1
+                    vaildValues[Duple(x: x+1, y: y)] = 1
                 }
                 // Left
                 if (mazze[y][x-1] == 0) {
-                    vaildValues[MyPoint(x: x-1, y: y)] = 1
+                    vaildValues[Duple(x: x-1, y: y)] = 1
                 }
                 // Down
                 if (mazze[y+1][x] == 0) {
-                    vaildValues[MyPoint(x: x, y: y+1)] = 1
+                    vaildValues[Duple(x: x, y: y+1)] = 1
                 }
-                mazeDistances[MyPoint(x: x, y: y)] = vaildValues
-                vaildValues = [MyPoint : Int]()
+                mazeDistances[Duple(x: x, y: y)] = vaildValues
+                vaildValues = [Duple : Int]()
             }
         }
     }
     return mazeDistances
 }
 
-var maze = ([[1, 1, 1, 1, 1, 1],
-             [1, 0, 0, 0, 0, 1],
-             [1, 0, 1, 0, 0, 1],
-             [1, 0, 0, 0, 1, 1],
-             [1, 0, 1, 0, 0, 1],
-             [1, 1, 1, 1, 1, 1]])
+//var maze = ([[1, 1, 1, 1, 1, 1],
+//             [1, 0, 0, 0, 0, 1],
+//             [1, 0, 1, 0, 0, 1],
+//             [1, 1, 1, 1, 1, 1]])
 
-var temp = matrixToDictionary(mazze: maze)
+let maze = ([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+            [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 
-for(i, j) in temp[MyPoint(x: 1, y: 2)]! {
-    print("i", i, "j", j)
-}
-
-// Breath first
-var start = [MyPoint(x: 0, y: 0)]
-var goal = [MyPoint(x: 10, y: 10)]
-
-var currentNode = [MyPoint]()
-var visitedNodes = [MyPoint]()
-var fronterNodes = [MyPoint]()
-var visitedNodeCount = 0
-var nodeAndParentNode = [MyPoint : MyPoint]()
-
-nodeAndParentNode = [start : MyPoint(x: 0, y: 0)]
-visitedNodes = [start]
-visitedNodeCount += 1
-currentNode = start
-
-while (currentNode != goal) {
-    if (currentNode, not in visitedNodes) {
-        visitedNodes.append(currentNode)
-        visitedNodeCount += 1
-    }
-    for (newNode, in (state_graph[currentNode])) {
-        if (newNode, not, in visitedNodes) {
-            if (newNode, not, in fronterNodes) {
-                fronterNodes.append(newNode)
-                nodeAndParentNode.update({newNode:currentNode})
+func breathFirstSearch(startSquare: Duple, goalSquare: Duple, gameBoard: [Duple : Dictionary<Duple, Int>]) {
+    var visitedSquares = [Duple]()
+    var fronterSquares = [Duple]()
+    var visitedSquareCount = 0
+    var squareAndParentSquare = [Duple : Duple]()
+    
+    squareAndParentSquare[startSquare] = Duple(x:1, y:1)
+    visitedSquares += [startSquare]
+    visitedSquareCount += 1
+    var currentSquare = startSquare
+    
+    while (currentSquare != goalSquare) {
+        if !(visitedSquares.contains(currentSquare)) {
+            visitedSquares += [currentSquare]
+            visitedSquareCount += 1
+        }
+    
+        for (newFronterSquare, _) in gameBoard[currentSquare]! {
+            if !(visitedSquares.contains(newFronterSquare)) {
+                if !(visitedSquares.contains(newFronterSquare)) {
+                    fronterSquares += [newFronterSquare]
+                    squareAndParentSquare[newFronterSquare] = currentSquare
+                }
             }
         }
+        currentSquare = fronterSquares[0]
+        fronterSquares.remove(at: 0)
     }
-    currentNode = fronterNodes[0]
-    fronterNodes.pop(0)
+    print("--", squareAndParentSquare)
 }
+
+breathFirstSearch(startSquare: Duple(x:1, y:1), goalSquare: Duple(x:10, y:10), gameBoard: matrixToDictionary(mazze: maze))
