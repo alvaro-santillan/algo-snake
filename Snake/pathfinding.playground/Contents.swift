7767334af02fb7200adb369175cb7b8bb3240082
Alvaro Santillan <afsm1995@live.com>
2020-02-17 19:40:51 -0700

    Depth First Search Complete

--- Snake/pathfinding.playground/Contents.swift
+++ Snake/pathfinding.playground/Contents.swift
@@ -66,7 +66,7 @@ var result = [Duple : Duple]()
 
 // https://developer.apple.com/documentation/swift/keyvaluepairs for ordered path
 func findPath(nodeAndParentNode: [Duple : Duple], end: Duple) -> [Duple : Duple] {
-    if (end == Duple(x:-1, y:-1)) {
+    if (end == Duple(x:-2, y:-2)) {
         return result
     } else {
         result[end] = nodeAndParentNode[end]
@@ -89,8 +89,43 @@ func breathFirstSearch(startSquare: Duple, goalSquare: Duple, gameBoard: [Duple 
     var fronterSquares = [Duple]()
     var visitedSquareCount = 0
     var squareAndParentSquare = [Duple : Duple]()
-    
+
     squareAndParentSquare[startSquare] = Duple(x:-1, y:-1)
+    visitedSquares += [startSquare]
+    visitedSquareCount += 1
+    var currentSquare = startSquare
+
+    while (currentSquare != goalSquare) {
+        if !(visitedSquares.contains(currentSquare)) {
+            visitedSquares += [currentSquare]
+            visitedSquareCount += 1
+        }
+
+        for (newFronterSquare, _) in gameBoard[currentSquare]! {
+            if !(visitedSquares.contains(newFronterSquare)) {
+                if !(visitedSquares.contains(newFronterSquare)) {
+                    fronterSquares += [newFronterSquare]
+                    squareAndParentSquare[newFronterSquare] = currentSquare
+                }
+            }
+        }
+        currentSquare = fronterSquares[0]
+        fronterSquares.remove(at: 0)
+    }
+    var bfsPath = findPath(nodeAndParentNode: squareAndParentSquare, end: goalSquare)
+    var bfspathCost = findPathCost(path: bfsPath, stateGraph: gameBoard)
+    print(bfspathCost)
+}
+
+//breathFirstSearch(startSquare: Duple(x:1, y:1), goalSquare: Duple(x:10, y:10), gameBoard: matrixToDictionary(mazze: maze))
+
+func depthFirstSearch(startSquare: Duple, goalSquare: Duple, gameBoard: [Duple : Dictionary<Duple, Int>]) {
+    var visitedSquares = [Duple]()
+    var fronterSquares = [Duple]()
+    var visitedSquareCount = 0
+    var squareAndParentSquare = [Duple : Duple]()
+    
+    squareAndParentSquare[startSquare] = Duple(x:-2, y:-2)
     visitedSquares += [startSquare]
     visitedSquareCount += 1
     var currentSquare = startSquare
@@ -109,12 +144,15 @@ func breathFirstSearch(startSquare: Duple, goalSquare: Duple, gameBoard: [Duple 
                 }
             }
         }
-        currentSquare = fronterSquares[0]
-        fronterSquares.remove(at: 0)
+        
+        currentSquare = fronterSquares.last!
+        fronterSquares.popLast()
     }
     var bfsPath = findPath(nodeAndParentNode: squareAndParentSquare, end: goalSquare)
     var bfspathCost = findPathCost(path: bfsPath, stateGraph: gameBoard)
-    print(bfspathCost)
+//    print(bfsPath)
+//    print(bfspathCost)
 }
 
-breathFirstSearch(startSquare: Duple(x:1, y:1), goalSquare: Duple(x:10, y:10), gameBoard: matrixToDictionary(mazze: maze))
+depthFirstSearch(startSquare: Duple(x:1, y:1), goalSquare: Duple(x:10, y:10), gameBoard: matrixToDictionary(mazze: maze))
+// 18,32,24,42,38,28,52
