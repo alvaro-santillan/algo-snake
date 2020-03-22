02e0acf1b951310aeddfdb90b2000e8117194869
Alvaro Santillan <afsm1995@live.com>
2020-03-21 19:14:48 -0600

    Help Screen Icon Formated.

--- Snake/Snake/Controllers/HelpScreenViewController.swift
+++ Snake/Snake/Controllers/HelpScreenViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class HelpViewController: UIViewController {
-
+    @IBOutlet weak var backToPreviousScreenButton: UIButton!
+    @IBOutlet weak var homeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        backToPreviousScreenButton.imageEdgeInsets = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
+        homeButton.imageEdgeInsets = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
     }
 
     @IBAction func rateButton(_ sender: UIButton) {
