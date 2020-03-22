88fc3cb25a2961383b51281cddd68e38f3671f39
Alvaro Santillan <afsm1995@live.com>
2020-03-22 13:22:51 -0600

    Help Screen Buttons And Shadows Implimented.

--- Snake/Snake/Controllers/HelpScreenViewController.swift
+++ Snake/Snake/Controllers/HelpScreenViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class HelpViewController: UIViewController {
     @IBOutlet weak var leftView: UIView!
     @IBOutlet weak var linkedInButton: UIButton!
+    @IBOutlet weak var websiteButton: UIButton!
     @IBOutlet weak var githubButton: UIButton!
     @IBOutlet weak var rateButton: UIButton!
     @IBOutlet weak var backToPreviousScreenButton: UIButton!
@@ -18,8 +19,31 @@ class HelpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Views
+        leftView.layer.shadowColor = UIColor.darkGray.cgColor
+        leftView.layer.shadowRadius = 10
+        leftView.layer.shadowOpacity = 0.5
+        leftView.layer.shadowOffset = .zero
+        
+        // Buttons
+        rateButton.imageEdgeInsets = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
+        rateButton.layer.cornerRadius = 6
+        rateButton.layer.shadowColor = UIColor.darkGray.cgColor
+        rateButton.layer.shadowRadius = 5
+        rateButton.layer.shadowOpacity = 0.2
+        rateButton.layer.shadowOffset = .zero
         backToPreviousScreenButton.imageEdgeInsets = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
+        backToPreviousScreenButton.layer.cornerRadius = 6
+        backToPreviousScreenButton.layer.shadowColor = UIColor.darkGray.cgColor
+        backToPreviousScreenButton.layer.shadowRadius = 5
+        backToPreviousScreenButton.layer.shadowOpacity = 0.2
+        backToPreviousScreenButton.layer.shadowOffset = .zero
         homeButton.imageEdgeInsets = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
+        homeButton.layer.cornerRadius = 6
+        homeButton.layer.shadowColor = UIColor.darkGray.cgColor
+        homeButton.layer.shadowRadius = 5
+        homeButton.layer.shadowOpacity = 0.2
+        homeButton.layer.shadowOffset = .zero
     }
 
     @IBAction func rateButton(_ sender: UIButton) {
