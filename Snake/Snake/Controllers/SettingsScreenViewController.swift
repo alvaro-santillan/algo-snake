0e5ac375e161c297dbb46cd0656b980bb1fec16f
Alvaro Santillan <afsm1995@live.com>
2020-06-12 14:58:51 -0600

    New Color Palette And The Game Board Has Unique Colors Depending On Dark Or Light Mode.

--- Snake/Snake/Controllers/SettingsScreenViewController.swift
+++ Snake/Snake/Controllers/SettingsScreenViewController.swift
@@ -77,27 +77,50 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     
     let defaults = UserDefaults.standard
     let colors = [
+        UIColor(red: 0.94, green: 0.33, blue: 0.31, alpha: 1.00), // Light Red
+        UIColor(red: 0.83, green: 0.18, blue: 0.18, alpha: 1.00), // Dark Red
+        UIColor(red: 0.93, green: 0.25, blue: 0.48, alpha: 1.00), // Light Pink
+        UIColor(red: 0.76, green: 0.09, blue: 0.36, alpha: 1.00), // Dark Pink
+        UIColor(red: 0.67, green: 0.28, blue: 0.74, alpha: 1.00), // Light Purple
+        UIColor(red: 0.48, green: 0.12, blue: 0.64, alpha: 1.00), // Dark Purple
+        UIColor(red: 0.49, green: 0.34, blue: 0.76, alpha: 1.00), // Light Deep Purple
+        UIColor(red: 0.32, green: 0.18, blue: 0.66, alpha: 1.00), // Dark Deep Purple
+        UIColor(red: 0.36, green: 0.42, blue: 0.75, alpha: 1.00), // Light Indigo
+        UIColor(red: 0.19, green: 0.25, blue: 0.62, alpha: 1.00), // Dark Indigo
+        UIColor(red: 0.26, green: 0.65, blue: 0.96, alpha: 1.00), // Light Blue
+        UIColor(red: 0.10, green: 0.46, blue: 0.82, alpha: 1.00), // Dark Blue
+        UIColor(red: 0.16, green: 0.71, blue: 0.96, alpha: 1.00), // Light Light Blue
+        UIColor(red: 0.01, green: 0.53, blue: 0.82, alpha: 1.00), // Dark Light Blue
+        UIColor(red: 0.15, green: 0.78, blue: 0.85, alpha: 1.00), // Light Cyan
+        UIColor(red: 0.00, green: 0.59, blue: 0.65, alpha: 1.00), // Dark Cyan
+        UIColor(red: 0.15, green: 0.65, blue: 0.60, alpha: 1.00), // Light Teal
+        UIColor(red: 0.00, green: 0.47, blue: 0.42, alpha: 1.00), // Dark Teal
+        UIColor(red: 0.40, green: 0.73, blue: 0.42, alpha: 1.00), // Light Green
+        UIColor(red: 0.22, green: 0.56, blue: 0.24, alpha: 1.00), // Dark Green
+        UIColor(red: 0.61, green: 0.80, blue: 0.40, alpha: 1.00), // Light Light Green
+        UIColor(red: 0.41, green: 0.62, blue: 0.22, alpha: 1.00), // Dark Light Green
+        UIColor(red: 0.83, green: 0.88, blue: 0.34, alpha: 1.00), // Light Lime
+        UIColor(red: 0.69, green: 0.71, blue: 0.17, alpha: 1.00), // Dark Lime
+        UIColor(red: 1.00, green: 0.93, blue: 0.35, alpha: 1.00), // Light Yellow
+        UIColor(red: 0.98, green: 0.75, blue: 0.18, alpha: 1.00), // Dark Yellow
+        UIColor(red: 1.00, green: 0.79, blue: 0.16, alpha: 1.00), // Light Amber
+        UIColor(red: 1.00, green: 0.63, blue: 0.00, alpha: 1.00), // Dark Amber
+        UIColor(red: 1.00, green: 0.65, blue: 0.15, alpha: 1.00), // Light Orange
+        UIColor(red: 0.96, green: 0.49, blue: 0.00, alpha: 1.00), // Dark Orange
+        UIColor(red: 1.00, green: 0.44, blue: 0.26, alpha: 1.00), // Light Deep Orange
+        UIColor(red: 0.90, green: 0.29, blue: 0.10, alpha: 1.00)] // Dark Deep Orange
+    let lightBackgroundColors = [
         UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.00), // White Clouds
         UIColor(red:0.74, green:0.76, blue:0.78, alpha:1.00), // White Silver
         UIColor(red:0.58, green:0.65, blue:0.65, alpha:1.00), // Light Gray Concrete
-        UIColor(red:0.50, green:0.55, blue:0.55, alpha:1.00), // Light Gray Asbestos
+        UIColor(red:0.50, green:0.55, blue:0.55, alpha:1.00)] // Light Gray Asbestos
+    let darkBackgroundColors = [
         UIColor(red:0.20, green:0.29, blue:0.37, alpha:1.00), // Dark Gray Wet Asphalt
         UIColor(red:0.17, green:0.24, blue:0.31, alpha:1.00), // Dark Gray Green Sea
-        UIColor(red:0.61, green:0.35, blue:0.71, alpha:1.00), // Purple Amethyst
-        UIColor(red:0.56, green:0.27, blue:0.68, alpha:1.00), // Purple Wisteria
-        UIColor(red:0.20, green:0.60, blue:0.86, alpha:1.00), // Blue Peter River
-        UIColor(red:0.16, green:0.50, blue:0.73, alpha:1.00), // Blue Belize Hole
-        UIColor(red:0.18, green:0.80, blue:0.44, alpha:1.00), // Green Emerald
-        UIColor(red:0.15, green:0.68, blue:0.38, alpha:1.00), // Green Nephritis
-        UIColor(red:0.10, green:0.74, blue:0.61, alpha:1.00), // Teal Turquoise
-        UIColor(red:0.09, green:0.63, blue:0.52, alpha:1.00), // Teal GreenSea
-        UIColor(red:0.91, green:0.30, blue:0.24, alpha:1.00), // Red Alizarin
-        UIColor(red:0.75, green:0.22, blue:0.17, alpha:1.00), // Red Pomegranate
-        UIColor(red:0.90, green:0.49, blue:0.13, alpha:1.00), // Orange Carrot
-        UIColor(red:0.83, green:0.33, blue:0.00, alpha:1.00), // Orange Pumpkin
-        UIColor(red:0.95, green:0.77, blue:0.06, alpha:1.00), // Yellow Sun Flower
-        UIColor(red:0.95, green:0.61, blue:0.07, alpha:1.00)] // Yellow Orange
-    var legendData = [["Snake Head", 0], ["Snake Body", 0], ["Food", 3], ["Path", 17], ["Visited Square", 5], ["Queued Square", 15], ["Barrier", 7], ["Weight", 19],  ["Gameboard", 13]]
+        UIColor(red:0.20, green:0.29, blue:0.37, alpha:1.00), // Dark Gray Wet Asphalt
+        UIColor(red:0.17, green:0.24, blue:0.31, alpha:1.00)] // Dark Gray Green Sea
+    
+    var legendData = [["Snake Head", 0], ["Snake Body", 0], ["Food", 3], ["Path", 17], ["Visited Square", 5], ["Queued Square", 15], ["Barrier", 7], ["Weight", 19],  ["Gameboard", 1]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -158,10 +181,20 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SettingsScreenTableViewCell
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
         tapGestureRecognizer.numberOfTapsRequired = 1
+        var appropritaeColorPalletArray = [UIColor]()
         
         cell.selectionStyle = UITableViewCell.SelectionStyle.none
         cell.legendOptionText.text = legendData[indexPath.row][0] as? String
-        cell.legendOptionSquareColor.backgroundColor = colors[(legendData[indexPath.row][1] as? Int)!]
+        if (legendData[indexPath.row][0] as? String) == "Gameboard" {
+            if defaults.bool(forKey: "Dark Mode On Setting") == true {
+                appropritaeColorPalletArray = darkBackgroundColors
+            } else {
+                appropritaeColorPalletArray = lightBackgroundColors
+            }
+        } else {
+            appropritaeColorPalletArray = colors
+        }
+        cell.legendOptionSquareColor.backgroundColor = appropritaeColorPalletArray[(legendData[indexPath.row][1] as? Int)!]
         cell.legendOptionSquareColor.layer.borderWidth = 1
         cell.legendOptionSquareColor.layer.cornerRadius = cell.legendOptionSquareColor.frame.size.width/4
         cell.legendOptionSquareColor.tag = indexPath.row
@@ -170,13 +203,29 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         return cell
     }
     
+    // perhapse as string and int can be removed.
     @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
         let tappedSquare = tapGestureRecognizer.view as! UIImageView
         var colorID = (legendData[tappedSquare.tag][1] as! Int) + 1
-        colorID == colors.count ? (colorID = 0) : ()
-        legendData[tappedSquare.tag][1] = colorID
-        defaults.set(legendData, forKey: "Legend Preferences")
-        defaults.setColor(color: colors[(legendData[tappedSquare.tag][1] as! Int)], forKey: legendData[tappedSquare.tag][0] as! String)
+        
+        if (legendData[tappedSquare.tag][0] as! String) == "Gameboard" {
+            if defaults.bool(forKey: "Dark Mode On Setting") == true {
+                colorID == darkBackgroundColors.count ? (colorID = 0) : ()
+                legendData[tappedSquare.tag][1] = colorID
+                defaults.set(legendData, forKey: "Legend Preferences")
+                defaults.setColor(color: darkBackgroundColors[(legendData[tappedSquare.tag][1] as! Int)], forKey: legendData[tappedSquare.tag][0] as! String)
+            } else {
+                colorID == lightBackgroundColors.count ? (colorID = 0) : ()
+                legendData[tappedSquare.tag][1] = colorID
+                defaults.set(legendData, forKey: "Legend Preferences")
+                defaults.setColor(color: lightBackgroundColors[(legendData[tappedSquare.tag][1] as! Int)], forKey: legendData[tappedSquare.tag][0] as! String)
+            }
+        } else {
+            colorID == colors.count ? (colorID = 0) : ()
+            legendData[tappedSquare.tag][1] = colorID
+            defaults.set(legendData, forKey: "Legend Preferences")
+            defaults.setColor(color: colors[(legendData[tappedSquare.tag][1] as! Int)], forKey: legendData[tappedSquare.tag][0] as! String)
+        }
         tableVIew.reloadData()
     }
     
@@ -231,6 +280,7 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     @IBAction func darkModeButtonPressed(_ sender: UIButton) {
         boolButtonResponder(sender, isIconButton: true, key: "Dark Mode On Setting", trueOption: "Dark_Mode_Icon.pdf", falseOption: "Light_Mode_Icon.pdf")
         defaults.bool(forKey: "Dark Mode On Setting") == true ? (overrideUserInterfaceStyle = .dark) : (overrideUserInterfaceStyle = .light)
+        tableVIew.reloadData()
     }
 
     func boolButtonResponder(_ sender: UIButton, isIconButton: Bool, key: String, trueOption: String, falseOption: String) {
