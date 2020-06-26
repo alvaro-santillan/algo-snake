649121cf240f9e2e03612e56f39bcca058465234
Alvaro Santillan <afsm1995@live.com>
2020-06-26 00:11:56 -0600

    GameScreen Score Button Now Changes Color When Tapped.

--- Snake/Snake/GameScene.swift
+++ Snake/Snake/GameScene.swift
@@ -15,8 +15,8 @@ class GameScene: SKScene {
     var foodPosition = [CGPoint]()
     var gameBackground: SKShapeNode!
     var gameBoard: [(node: SKShapeNode, x: Int, y: Int)] = []
-    let rowCount = 7 // 17
-    let columnCount = 7 // 30
+    let rowCount = 17 // 17
+    let columnCount = 30 // 30
     
     var snakeHeadSquareColor = UIColor() // "Snake Head"
     var snakeBodySquareColor = UIColor() // "Snake Body"
