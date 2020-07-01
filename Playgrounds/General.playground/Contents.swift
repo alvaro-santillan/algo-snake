5fb96178c20c5f34bafa9d36ed3a361ae85957aa
Alvaro Santillan <afsm1995@live.com>
2020-06-30 23:52:22 -0600

    Folder Restructure

---
+++ Playgrounds/General.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+import SpriteKit
+var a = SKAction()
+var b = SKAction()
+
+a = .wait(forDuration: TimeInterval(1.0) * 0.025)
+b = a
+
+//visitedSquareWait = .wait(forDuration: TimeInterval(squareIIndex) * 0.025)
+
+print(a, b)
--- Snake/Snake/Controllers/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  Snake
-//
-//  Created by Álvaro Santillan on 6/15/20.
-//  Copyright © 2020 Álvaro Santillan. All rights reserved.
-//
-
-import Foundation
